bd80a5d2efa0b78bbb304b78eeb5187e818ae754
masanoritakeda <t.masanori0712@icloud.com>
2022-09-26 09:22:01 +0900

    heroku修正２

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    
+    config.assets.initialize_on_precompile = false
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
